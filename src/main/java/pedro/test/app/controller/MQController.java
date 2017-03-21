d126013b86290ec538cdd37666060dddbfae3566
Antipodas <pedro.algaba@gmail.com>
2017-03-21 10:04:45 +0300

    My First commit

---
+++ src/main/java/pedro/test/app/controller/MQController.java
@@ -0,0 +1,27 @@
+package pedro.test.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pedro.test.app.properties.MQProperties;
+
+@RestController
+public class MQController {
+	
+	@Autowired
+	MQProperties mymq;
+	
+	
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+    
+    @RequestMapping("/MQ")
+    public String MQ(){
+    	return  mymq.toString();
+    }
+    
+
+}
